ef227a8c9e4aa03f65e2b2f67cac5f6734663162
zuzanad <zuzanad@kentico.com>
2018-09-17 16:13:30 +0200

    Redux - Install redux and create store with initial state

--- 05-redux/src/App.tsx
+++ 05-redux/src/App.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react';
+import { createStore } from 'redux';
 import { TodoApp } from './todoApp/components/TodoApp';
 import { Navigation } from './common/components/Navigation';
+import { rootReducer } from './common/rootReducer';
+import { getInitialItems } from './todoApp/utils/getInitialItems';
+
+const initialState = {
+  todoApp: {
+    items: getInitialItems(),
+  }
+};
+
+const store = createStore(rootReducer, initialState);
+
+console.log(store.getState());
+console.log(store.getState().todoApp.items.toJS());
 
 export class App extends React.PureComponent {
   render() {
