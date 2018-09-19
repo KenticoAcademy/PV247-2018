6281306d90a23914e5a722a1e89166a7855b25c6
zuzanad <zuzanad@kentico.com>
2018-09-19 16:29:28 +0200

    Redux - Use reselect for selector memoization

--- 05-redux/src/todoApp/containers/TodoList.tsx
+++ 05-redux/src/todoApp/containers/TodoList.tsx
@@ -1,30 +1,36 @@
 import * as Immutable from 'immutable';
-import * as memoize from 'memoizee';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { IState } from '../../common/IState';
 import { ITodoListStateProps, TodoList } from '../components/TodoList';
 import { TodoFilter } from '../constants/TodoFilter';
 import { ITodoItem } from '../models/ITodoItem';
 
-const getVisibleTodoIds = memoize((visibilityFilter: TodoFilter, allIds: Immutable.List<Uuid>, byId: Immutable.Map<Uuid, ITodoItem>): Immutable.List<Uuid> => {
-  switch (visibilityFilter) {
-    case TodoFilter.All:
-      return allIds;
+const getVisibleTodoIds = createSelector<IState, TodoFilter, Immutable.List<Uuid>, Immutable.Map<Uuid, ITodoItem>, Immutable.List<Uuid>>(
+  [
+    state => state.todoApp.visibilityFilter,
+    state => state.todoApp.items.allIds,
+    state => state.todoApp.items.byId,
+  ],
+  (visibilityFilter, allIds, byId) => {
+    switch (visibilityFilter) {
+      case TodoFilter.All:
+        return allIds;
 
-    case TodoFilter.Done:
-      return allIds.filter((id: Uuid) => byId.get(id).isCompleted).toList();
+      case TodoFilter.Done:
+        return allIds.filter((id: Uuid) => byId.get(id).isCompleted).toList();
 
-    case TodoFilter.Todo:
-      return allIds.filter((id: Uuid) => !byId.get(id).isCompleted).toList();
+      case TodoFilter.Todo:
+        return allIds.filter((id: Uuid) => !byId.get(id).isCompleted).toList();
 
-    default:
-      throw new Error(`Unknown value of visibility filter '${visibilityFilter}'`);
-  }
-});
+      default:
+        throw new Error(`Unknown value of visibility filter '${visibilityFilter}'`);
+    }
+  });
 
 const mapStateToProps = (state: IState): ITodoListStateProps => {
   return {
-    todoIds: getVisibleTodoIds(state.todoApp.visibilityFilter, state.todoApp.items.allIds, state.todoApp.items.byId),
+    todoIds: getVisibleTodoIds(state),
   };
 };
 
