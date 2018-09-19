d480a0df35da2a5906083a47c59ae940413e4aff
zuzanad <zuzanad@kentico.com>
2018-09-19 16:02:35 +0200

    Redux - Implement dummy selector for visible items

--- 05-redux/src/todoApp/containers/TodoList.tsx
+++ 05-redux/src/todoApp/containers/TodoList.tsx
@@ -1,10 +1,28 @@
+import * as Immutable from 'immutable';
 import { connect } from 'react-redux';
 import { IState } from '../../common/IState';
 import { ITodoListStateProps, TodoList } from '../components/TodoList';
+import { TodoFilter } from '../constants/TodoFilter';
+
+const getVisibleTodoIds = (state: IState): Immutable.List<Uuid> => {
+  switch (state.todoApp.visibilityFilter) {
+    case TodoFilter.All:
+      return state.todoApp.items.allIds;
+
+    case TodoFilter.Done:
+      return state.todoApp.items.allIds.filter((id: Uuid) => state.todoApp.items.byId.get(id).isCompleted).toList();
+
+    case TodoFilter.Todo:
+      return state.todoApp.items.allIds.filter((id: Uuid) => !state.todoApp.items.byId.get(id).isCompleted).toList();
+
+    default:
+      throw new Error(`Unknown value of visibility filter '${state.todoApp.visibilityFilter}'`);
+  }
+};
 
 const mapStateToProps = (state: IState): ITodoListStateProps => {
   return {
-    todoIds: state.todoApp.items.allIds,
+    todoIds: getVisibleTodoIds(state),
   };
 };
 
