d607d5965980a572c2a54cccdd2a5076f1d072c2
Vilém Jeniš <vilemj@kentico.com>
2018-10-23 09:02:05 +0200

    Add 6th exercise.

---
+++ Lecture_6_AsyncOperations/ex6.ts
@@ -0,0 +1,13 @@
+import './bootstrap';
+import * as fetch from 'node-fetch';
+
+/*
+ * use 'fetch' to get a user from 'https://randomuser.me/api/' and console log their name.
+ */
+
+async function run() {
+  
+  console.log(name);
+}
+
+run();
