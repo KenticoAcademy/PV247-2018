632e1de58b634d93dd81351b4a484ba188d6bbf9
PetrSvirak <PetrS2@kentico.com>
2018-11-01 09:43:07 +0100

    Router - revert to "some layout"
    webpack dev-server fallback removed as well

--- Lecture_8/src/components/content/Content.tsx
+++ Lecture_8/src/components/content/Content.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import {ManualPathRouter} from './manualRouting/ManualPathRouter';
 
 export const Content: React.StatelessComponent = () => (
   <div className="container">
     <div className="jumbotron">
-      <ManualPathRouter />
+      <h1>Routing</h1>
+      <p>
+        Such layout. Much wow. Very amaze. So swag.
+      </p>
+      <p>
+        <a className="btn btn-lg btn-primary" href="/form.html" role="button">Fill in</a>
+      </p>
     </div>
   </div>
 );
--- Lecture_8/webpack.config.js
+++ Lecture_8/webpack.config.js
@@ -90,7 +90,6 @@ module.exports = {
     open: true,
     quiet: false,
     noInfo: false,
-    historyApiFallback: true,
     before(app) {
       // mock server response
       app.post('*', (_req, res, next) => {
