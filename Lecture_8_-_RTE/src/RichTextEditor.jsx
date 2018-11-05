155b89a10b998772eeee61e82b869d1c4480fea7
Patrik Medveď <patrikm@kentico.com>
2018-11-05 12:45:54 +0100

    Lecture_9 Add bold text functionality

--- Lecture_8_-_RTE/src/RichTextEditor.jsx
+++ Lecture_8_-_RTE/src/RichTextEditor.jsx
@@ -1,4 +1,4 @@
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import React from 'react';
 
 export class RichTextEditor extends React.PureComponent {
@@ -12,6 +12,22 @@ export class RichTextEditor extends React.PureComponent {
     }));
   };
 
+  _handleKeyCommand = (command, editorState) => {
+    if (command === 'bold') {
+      const newState = RichUtils.handleKeyCommand(editorState, command);
+      if (newState) {
+        this._onChange(newState);
+        return 'handled';
+      }
+    }
+
+    return 'not-handled';
+  };
+
+  _onBoldButtonClicked = () => {
+    this._onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  };
+
   render() {
     return (
       <div
@@ -21,9 +37,21 @@ export class RichTextEditor extends React.PureComponent {
           padding: '8px'
         }}
       >
+        <div>
+          <button onClick={this._onBoldButtonClicked}>
+            Bold
+          </button>
+          <div
+            style={{
+              margin: '4px 0',
+              borderTop: '1px solid #333'
+            }}
+          />
+        </div>
         <Editor
           editorState={this.state.editorState}
           onChange={this._onChange}
+          handleKeyCommand={this._handleKeyCommand}
           // blockRendererFn={}
           // blockStyleFn={}
           // keyBindingFn={}
